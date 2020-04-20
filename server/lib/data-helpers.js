13d1043258c01a500aeb4deb482daea75c07c99f
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-20 09:49:54 -0700

    fixed up some small things from cloned file

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -4,7 +4,7 @@
 const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
-module.exports = function makeDataHelpers(db) {
+const makeDataHelpers = (db) => {
   return {
 
     // Saves a tweet to `db`
@@ -18,10 +18,12 @@ module.exports = function makeDataHelpers(db) {
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        const sortNewestFirst = (a, b) => a.createdAt - b.createdAt;
         callback(null, db.tweets.sort(sortNewestFirst));
       });
     }
 
   };
-}
+};
+
+module.exports = makeDataHelpers;
